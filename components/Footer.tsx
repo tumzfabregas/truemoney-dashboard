73ce9fffe15173119dd6795e778c109b4c297b05
natthawut-tumz <tumz.natthawut@gmail.com>
2025-11-25 17:34:25 +0700

    update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="border-t border-gray-700 bg-[#1a2234] py-4 mt-auto">
-      <div className="text-center text-xs text-white">
-        Powered by <span className="font-bold">XYZ</span>
+    <footer className="border-t border-[#444746] bg-[#1E1F20] py-4 mt-auto">
+      <div className="text-center text-xs text-slate-400">
+        Powered by <span className="font-bold text-slate-200">XYZ</span>
       </div>
     </footer>
   );
